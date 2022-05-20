7b9e491d82609a79d68044574d04535c4748eed9
chiseong kim <dlapdlf1013@gmail.com>
2022-05-20 20:00:49 +0900

    수정사항

--- Miniproject/src/main/java/MytestJava.java
+++ Miniproject/src/main/java/MytestJava.java
@@ -4,7 +4,7 @@ public class MytestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("원영이형 너무 감사드립니다 (_ _)");
+		System.out.println("원영이형 굿");
 		
 	}
 
